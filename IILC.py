aa12acd01aa497f1ce105164464b1b6fc6cb6eff
Kenneth Loebenberg <kloebenberg@gmail.com>
2018-06-24 16:35:21 -0500

    Update IILC.py

--- IILC.py
+++ IILC.py
@@ -1,213 +1,50 @@
+from sys import version_info
 import os, csv
 
-if os.name == 'nt':
+py3 = version_info[0] > 2 #creates boolean value for test that Python major version > 2
+
+if py3:
+    what = input("Do you want files or paths:")
+    path = input("Enter Path: ")
+    name = input("Enter Project Name: ")
+    if os.name != 'nt':
+        drive = input("What is the name of your drive:")
+        indsign = input("You want me to prep this for InDesign? Y or N: ")
+else:
     what = raw_input("Do you want files or paths:")
     path = raw_input("Enter Path: ")
     name = raw_input("Enter Project Name: ")
-    
-    if what == "FILES":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([filename])
-                
-    if what == "FILE":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([filename])
-                
-    if what == "PATHS":  
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([path+filename])
-        
-    if what == "PATH":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([path+filename])
-    if what == "Files":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([filename])
-                
-    if what == "File":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([filename])
-                
-    if what == "Paths":  
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([path+filename])
-        
-    if what == "Path":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([path+filename])
-    if what == "files":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([filename])
-                
-    if what == "file":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([filename])
-                
-    if what == "paths":  
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([path+filename])
-        
-    if what == "path":
-        csvOut=open(name+".csv",'w')
-        w=csv.writer(csvOut)
-        for path, dirs, files in os.walk(path):
-            for filename in files:
-                w.writerow([path+filename])
+    if os.name != 'nt':
+        drive = raw_input("What is the name of your drive:")
+        indsign = raw_input("You want me to prep this for InDesign? Y or N: ")
 
-else:
-    macwhat = raw_input("Do you want files or paths:")
-    macpath = raw_input("Enter Path: ")
-    macdoc = raw_input("Enter Project Name: ")
-    macdrive = raw_input("what is the name of your Drive:")   
-    if macwhat == "FILES":
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([filename])
-                csvOut.close()
-    if macwhat == "FILE":
-       csvOut=open(macdoc+".csv",'w')
-       w=csv.writer(csvOut)
-       for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([filename])
-    if macwhat == "Files":
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([filename])                
-    if macwhat == "File":
-       csvOut=open(macdoc+".csv",'w')
-       w=csv.writer(csvOut)
-       for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([filename])                
-    if macwhat == "files":
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([filename])               
-    if macwhat == "file":
-       csvOut=open(macdoc+".csv",'w')
-       w=csv.writer(csvOut)
-       for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([filename])
-    elif macwhat == "PATHS":  
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([macdrive+macpath+"/"+filename])
-     
-    elif macwhat == "PATH":
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([macdrive+macpath+"/"+filename])
-    elif macwhat == "Paths":  
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([macdrive+macpath+"/"+filename])
-     
-    elif macwhat == "Path":
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([macdrive+macpath+"/"+filename])
-    elif macwhat == "paths":  
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([macdrive+macpath+"/"+filename])
-     
-    elif macwhat == "path":
-        csvOut=open(macdoc+".csv",'w')
-        w=csv.writer(csvOut)
-        for macpath, dirs, files in os.walk(macpath):
-            for filename in files:
-                w.writerow([macdrive+macpath+"/"+filename])
-                
-     #else ==             
-indsign = raw_input("you want me to prep this for indesign? Y or N")
+what = what.lower()
+csvfile = os.path.join(path, name + ".csv")
+changes = {   # a dictionary of changes to make, find 'key' substitue with 'value'
+        '/' : ':', # I assume both 'key' and 'value' are strings
+        }
 
-if indsign == "Y":
-    new_rows = [] # a holder for our modified rows when we make them
-    changes = {   # a dictionary of changes to make, find 'key' substitue with 'value'
-            '/' : ':', # I assume both 'key' and 'value' are strings
-            }
-    with open(macdoc+'.csv', 'rb') as f:
-        reader = csv.reader(f) # pass the file to our csv reader
-        for row in reader:     # iterate over the rows in the file
-            new_row = row      # at first, just copy the row
-            for key, value in changes.items(): # iterate over 'changes' dictionary
-               new_row = [ x.replace(key, value) for x in new_row ] # make the substitutions
-            new_rows.append(new_row) # add the modified rows
-    with open(macdoc+'.csv', 'wb') as f:
-        # Overwrite the old file with the modified rows
-        writer = csv.writer(f)
-        writer.writerows(new_rows)
-if indsign == "y":
-    new_rows = [] # a holder for our modified rows when we make them
-    changes = {   # a dictionary of changes to make, find 'key' substitue with 'value'
-            '/' : ':', # I assume both 'key' and 'value' are strings
-            }
-    with open(macdoc+'.csv', 'rb') as f:
-        reader = csv.reader(f) # pass the file to our csv reader
-        for row in reader:     # iterate over the rows in the file
-            new_row = row      # at first, just copy the row
-            for key, value in changes.items(): # iterate over 'changes' dictionary
-               new_row = [ x.replace(key, value) for x in new_row ] # make the substitutions
-            new_rows.append(new_row) # add the modified rows
-    with open(macdoc+'.csv', 'wb') as f:
-        # Overwrite the old file with the modified rows
-        writer = csv.writer(f)
-        writer.writerows(new_rows)
-
-#!/usr/bin/env python
-
-        
-#if os.name == 'nt':
-#    pass
-#else:
+csvOut=open(csvfile,'w')
+w=csv.writer(csvOut, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+for path, dirs, files in os.walk(path):
+    for filename in files:
+        if what == "file" or what == "files" :
+            w.writerow([filename.rstrip()])
+        else:
+            if os.name == 'nt':
+                w.writerow([os.path.join(path, filename).rstrip()])
+            else:
+                data = drive + os.path.join(path, filename).rstrip()
+                if indsign == "Y" or indsign == "y":
+                    for key, value in changes.items(): # iterate over 'changes' dictionary
+                        data = data.replace(key, value) # make the substitutions
+                w.writerow([data])
+csvOut.close()
+path = raw_input("Enter Path Again: ")
+with open ("Final "+name+'.csv', 'wb') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerow(["@pic"])
+  
+    with open (path+"/"+name+'.csv', 'rb') as incsv:
+        reader = csv.reader(incsv)
+        writer.writerows(row + [] for row in reader)
